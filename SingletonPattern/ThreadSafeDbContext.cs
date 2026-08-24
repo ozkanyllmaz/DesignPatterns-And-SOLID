9341787338ef6029cf6cb708d008b797b262d8ac
Özkan Yılmaz <dev.ozkanyilmaz@gmail.com>
2026-08-24 10:24:51 +0300

    feat: Singleton Pattern added with singleton, lazy singleton and thread-safe singleton

---
+++ SingletonPattern/ThreadSafeDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class ThreadSafeDbContext
+    {
+        private static ThreadSafeDbContext? context;
+
+        private static readonly object _lock = new object();
+
+        private ThreadSafeDbContext()
+        {
+            Console.WriteLine("thread-safe context nesnesi üretildi.");
+        }
+
+        public static ThreadSafeDbContext GetInstance()
+        {
+            if(context == null)
+            {
+                // içeriye giren ilk kişi kapıyı kilitler diğerleri sırada bekler.
+                lock(_lock)
+                {
+                    // kapıda bekleyen içeri girdiğinde nesne 1.kişi tarafından üretilip üretilmediğini kontrol eder.
+                    if(context == null)
+                    {
+                        context = new ThreadSafeDbContext();
+                    }
+                }
+            }
+            return context;
+        }
+    }
+}
