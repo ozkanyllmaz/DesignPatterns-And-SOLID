029e4c00b8a91d595318f4c2f1cfd14b93ad9bdb
Özkan Yılmaz <dev.ozkanyilmaz@gmail.com>
2026-08-24 14:50:05 +0300

    feat: Factory Pattern added with badcode and goodcode

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿//using FactoryPattern.BadCode;
+
+//Console.WriteLine("BadCode Örneği");
+//Console.WriteLine("------------------");
+
+//LogisticApp logisticApp = new LogisticApp();
+//logisticApp.PlanDelivery("road");
+
+
+
+//-----------------------------------------------------------
+
+using FactoryPattern.BadCode;
+using FactoryPattern.GoodCode;
+
+Console.WriteLine("GoodCode Örneği");
+Console.WriteLine("------------------");
+
+Console.WriteLine("--- Karayolu Siparişi ---");
+Logistics roadManager = new RoadLogistics();
+roadManager.PlanDelivery();
+
+Console.WriteLine("--- Denizyolu Siparişi ---");
+Logistics seaManager = new SeaLogistics();
+seaManager.PlanDelivery();
+
