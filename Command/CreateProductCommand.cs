db08319d8f51d921b1899fa97fbf622c3cc3dc21
Özkan Yılmaz <dev.ozkanyilmaz@gmail.com>
2026-08-26 11:14:20 +0300

    feat: Command Pattern added

---
+++ Command/CreateProductCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Command
+{
+    public class CreateProductCommand : ICommand
+    {
+        private readonly ProductReceiver _productReceiver;
+        private readonly string _productName;
+        private readonly decimal _price;
+
+        public CreateProductCommand(ProductReceiver productReceiver, string productName, decimal price)
+        {
+            _productReceiver = productReceiver;
+            _productName = productName;
+            _price = price;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Komut çalışıyor: Ürün ekleme isteği doğrulandı.");
+            _productReceiver.InsertToDatabase(_productName, _price);
+        }
+    }
+}
