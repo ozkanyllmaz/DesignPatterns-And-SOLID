1f4ba69904c3ade9aab361882122bc5c6f423561
Özkan Yılmaz <dev.ozkanyilmaz@gmail.com>
2026-08-26 10:02:36 +0300

    feat: Strategy Pattern added

---
+++ Strategy/CreditCartPayment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    public class CreditCartPayment : IPaymentStrategy
+    {
+        public void ProcessPayment(decimal amount)
+            => Console.WriteLine($"{amount} TL Kredi kartı ile ödendi");
+    }
+}
---
+++ Strategy/CryptoStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy
+{
+    public class CryptoStrategy : IPaymentStrategy
+    {
+        public void ProcessPayment(decimal amount)
+            => Console.WriteLine($"{amount} TL Kripto ödeme yöntemiyle ödendi");
+    }
+}
